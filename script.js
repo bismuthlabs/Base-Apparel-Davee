920de9e7461d18e7548a0552d98d4f15c5d840c8
bismuthlabs <117520856+bismuthlabs@users.noreply.github.com>
2022-12-12 08:20:34 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('.input')
+
+function button() {
+    if (input.displayContent === 'none') {
+        button.style.display = 'none'
+        
+    }else{
+        button.style.visibility = 'block'
+    }
+    
+}
